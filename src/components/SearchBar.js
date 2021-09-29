61cec4eca6299d93c2ad8ac044bf8e3afa0dd42b
Paul Shin <paulkisooshin@gmail.com>
2021-09-29 15:41:05 -0700

    arrow bind onFormSubmit

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -8,11 +8,11 @@ class SearchBar extends React.Component {
 
   state = { term: "" };
 
-  onFormSubmit(event) {
+  onFormSubmit = (event) => {
     event.preventDefault();
 
     console.log(this.state.term);
-  }
+  };
 
   render() {
     return (
